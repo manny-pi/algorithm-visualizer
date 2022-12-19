fa26e298193a4c9639962778b9d5a28edfbe24c6
Prince Okpoziakpo <princeokpoziakpo@gmail.com>
2022-12-19 17:13:23 -0500

    Implemented selection sort

--- visualizer/Visualizer.py
+++ visualizer/Visualizer.py
@@ -2,15 +2,15 @@
 
 """
 
-import __algorithms.BubbleSort as BubbleSort
-import __algorithms.InsertionSort as InsertionSort
-import __algorithms.MergeSort as MergeSort
-import __algorithms.SelectionSort as SelectionSort
+from __algorithms.bubble_sort import BubbleSort
+from __algorithms.insertion_sort import InsertionSort
+from __algorithms.merge_sort import MergeSort
+from __algorithms.selection_sort import SelectionSort
 
 
 class Visualizer: 
 
-    def __init__(): 
+    def __init__(self): 
         self.__dataset = None          # dataset to be sorted and visualized
         self.encodedDataset = None     # color-coded dataset to be passed into the algorithm
         self.__algorithm = None        # the algorithm being used to sort
@@ -18,6 +18,9 @@ class Visualizer:
     def setDataset(self, dataset): 
         """Stores the raw values of the dataset being sorted."""
         
+        if dataset == None or dataset == []: 
+            raise TypeError("Type Error: dataset cannot be null. Please use the {Visualizer}.setDataset() function.")
+
         self.__dataset = list(dataset)  # deep-copy the dataset
         self.encodedDataset = self.__encodeDataset(self.__dataset)
 
@@ -40,20 +43,27 @@ class Visualizer:
 
         self.__render()
 
-    def __encodeDataset(self): 
+    def __encodeDataset(self, dataset): 
         """Color codes the values in the dataset."""
 
-        if self.__dataset == None or self.__dataset == []: 
-            raise TypeError("Type Error: dataset cannot be null. Please use the {Visualizer}.setDataset() function")
+        raise NotImplementedError("__encodeDataset() hasn't been implemented.")
 
     def __render(self): 
-        """Renders the color-coded dataset to the screen"""
-        pass 
+        """Renders the color-coded dataset to the screen."""
+
+        raise NotImplementedError("__render() hasn't been implemented.")
 
 
 class ColorCoded: 
-    """Represents the components that are used for the visualization"""
+    """Represents the components that are used for the visualization."""
 
     def __init__(self, value, color): 
         self.value = value
         self.color = color
+
+
+if __name__ == '__main__': 
+    from random import randint as r
+    viz = Visualizer()
+    viz.setDataset([r(1, 100) for i in range(50)])
+    viz.setAlgorithm(algorithm="selection_sort")
---
+++ visualizer/__algorithms/selection_sort.py
@@ -0,0 +1,39 @@
+from . import algorithm
+
+
+class SelectionSort(algorithm.Algorithm): 
+    def __init__(self, encodedDataset): 
+        """Initialize the outer variable for the selection sort method."""
+        
+        self.encodedDataset = encodedDataset
+        self.j = 0
+
+    def nextStep(self): 
+        """Completes a single step in the Selection Sort algorithm."""
+        
+        smallest = self.encodedDataset[self.j].value
+        indexOfSmallest = self.j
+        i = self.j + 1
+        while i < len(self.encodedDataset):
+            if self.encodedDataset[i].value < smallest.value:
+                smallest = self.encodedDataset[i]
+                indexOfSmallest = i
+            i += 1
+
+        current = self.encodedDataset[self.j]
+        self.encodedDataset[self.j] = smallest
+        self.encodedDataset[indexOfSmallest] = current
+        self.j += 1
+
+    def sorted(self):
+        """Check if the dataset is sorted. 
+        
+        Return True if the dataset is sorted. 
+        Return False if the dataset is not sorted."""
+
+        for i in range(len(self.encodedDataset) - 1): 
+            a = self.encodedDataset[i]
+            b = self.encodedDataset[i+1]
+            if not a <= b: 
+                return False
+        return True
